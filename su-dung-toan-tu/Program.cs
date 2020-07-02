a356eb03cfb7a379f153449b3f5661854a8fa32d
khoakids <anhkhoahihi82@gmail.com>
2020-07-02 14:32:23 +0700

    tính diện tích

---
+++ su-dung-toan-tu/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace su_dung_toan_tu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float width;
+            float height;
+            Console.WriteLine("Enter Width: ");
+            width = float.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Height: ");
+            height = float.Parse(Console.ReadLine());
+            float area = width * height;
+            Console.WriteLine("Area is " + area);
+        }
+    }
+}
