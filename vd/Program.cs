dcaa13379ce35b04e8229299fcb787d41d02a9da
khoakids <anhkhoahihi82@gmail.com>
2020-07-02 14:03:25 +0700

    c

---
+++ vd/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace vd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int total = 1;
+           int index = 1;
+           do {
+               for (int i = 0 ; i < 6 ; i++) {
+                   index = i;
+                   total += index;
+               }
+           } while (index < 5);
+           {
+               Console.WriteLine(total);
+           }
+        }
+    }
+}
