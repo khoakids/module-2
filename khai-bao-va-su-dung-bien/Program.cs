592ab686ec428195522709ad4b00774b27d6917a
khoakids <anhkhoahihi82@gmail.com>
2020-07-03 11:16:15 +0700

    khai báo và sử dụng biến

---
+++ khai-bao-va-su-dung-bien/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace khai_bao_va_su_dung_bien
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int i = 10;
+           float f = 20.5f;
+           double d = 20.5;
+           Boolean b = true;
+           char c = 'a';
+           string s = "CodeGym";
+           Console.WriteLine("giá trị của biến i là" + i);
+           Console.WriteLine("giá trị của biến f là" + f);
+           Console.WriteLine("giá trị của biến d là" + d);
+           Console.WriteLine("giá trị của biến b là" + b);
+           Console.WriteLine("giá trị của biến c là" + c);
+           Console.WriteLine("giá trị của biến s là" + s);
+        }
+    }
+}
