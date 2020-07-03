ff83ca458037b9c7cfe1540424a6987390dca4ed
khoakids <anhkhoahihi82@gmail.com>
2020-07-03 11:42:01 +0700

    hiển thị lời chào

---
+++ hien-thi-loi-chao/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace hien_thi_loi_chao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name");
+            string yourName = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Hello: " + yourName);
+        }
+    }
+}
